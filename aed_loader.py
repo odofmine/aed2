0fe5241c54c8eec3ae9052cf42466549ba7b0c60
anonymouse <anonymouse@x.com>
2021-03-22 14:59:56 +0800

    update

--- aed_loader.py
+++ aed_loader.py
@@ -6,11 +6,19 @@ class AedLoader:
     def write(self):
         now = datetime.utcnow().strftime('%Y-%m-%d')
         data = Utils.read_json(Config.aed_dir(), now)
-        self.write_balance(data)
+        key = 'trendfund-t4'
+        self.write_balance(key, data)
 
-    def write_balance(self, data):
+        key = 'jiuyao-91_2106'
+        data = Utils.read_json(f'{Config.aed_dir()}/jiuyao', now)
+        self.write_balance(key, data)
+
+    def write_exchanges_info():
+        pass
+
+    def write_balance(self, key, data):
         columns = ['datetime', 'balance']
         balances = [[int(x[0][0] / 1000), int(x[0][1])] for x in data]
-        Utils.write_file(f"{Config.root()['target_dir']}/data", 'balances_t4', balances, columns)
+        Utils.write_file(f"{Config.root()['target_dir']}/data", f'balances_{key}', balances, columns)
 
 AedLoader().write()
